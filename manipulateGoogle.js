77e14bff436be87afc1d375f195b38f172355a91
Frank Hale <frankhale@gmail.com>
2017-05-28 22:33:48 -0400

    Remove comment at top of manipulateGoogle.js
    - This comment no longer applies since technique to inject jQuery has
    now changed

--- manipulateGoogle.js
+++ manipulateGoogle.js
@@ -1,5 +1,3 @@
-// injection technique borrowed from http://stackoverflow.com/questions/840240/injecting-jquery-into-a-page-fails-when-using-google-ajax-libraries-api
-
 function addJQueryScript() {
   var script = document.createElement("script");
   script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
