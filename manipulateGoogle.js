7123a8f6019bf6331314105b22aec4af55e04dbe
Michael <32310492+michaelgv@users.noreply.github.com>
2017-09-28 11:01:35 -0400

    change to listen on DOMContentLoaded event
    It works... and it's better.

--- manipulateGoogle.js
+++ manipulateGoogle.js
@@ -1,17 +1,10 @@
-function addJQueryScript() {
-  var script = document.createElement("script");
-  script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
-  script.onload = script.onreadystatechange = function() {
-    $(document).ready(() => {
-      $("#lst-ib").val("Hello, World!");
+document.addEventListener("DOMContentLoaded", function(event) {
+    var script = document.createElement("script");
+    script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+    script.onload = script.onreadystatechange = function() {
+        $(function() {
+            $("#lst-ib").val("Hello, World!");
+        });
     });
-  };
-  document.body.appendChild(script);
-}
-
-let scriptAddInterval = setInterval(() => {
-  if (document.readyState === "complete") {
-    addJQueryScript();
-    clearInterval(scriptAddInterval);
-  }
+    document.appendChild(script); 
 });
